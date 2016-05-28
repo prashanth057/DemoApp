55eea420f11ba0bf52a3b7269945a4976be4be92
prashanthpachatu <prashanth057@gmail.com>
2016-05-28 21:41:09 +0530

    initial project commit

---
+++ src/main/java/com/pachap/demo/DemoApp.java
@@ -0,0 +1,13 @@
+package com.pachap.demo;
+
+/**
+ * Created by prashanthpachatu on 28/05/16.
+ */
+public class DemoApp {
+
+
+    public  static void main(String args[]){
+        System.out.println(" Hello Pachap!!");
+    }
+
+}
